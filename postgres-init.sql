ed8ca465b949e7f6603a09943cb1041fd37f483b
konradbjk <konradbjk@gmail.com>
2025-05-08 18:28:54 +0200

    adding n8n

--- postgres-init.sql
+++ postgres-init.sql
@@ -1,11 +1,11 @@
 -- Create two custom users
-CREATE USER user1 WITH PASSWORD 'pass1';
+CREATE USER app_n8n WITH PASSWORD 'n8n123';
 CREATE USER user2 WITH PASSWORD 'pass2';
 
 -- Create database db1 owned by user1, then lock down public and grant all rights
-CREATE DATABASE db1 OWNER user1;
-REVOKE CONNECT ON DATABASE db1 FROM PUBLIC;
-GRANT ALL PRIVILEGES ON DATABASE db1 TO user1;
+CREATE DATABASE n8n OWNER app_n8n;
+REVOKE CONNECT ON DATABASE n8n FROM PUBLIC;
+GRANT ALL PRIVILEGES ON DATABASE n8n TO app_n8n;
 
 -- Create database db2 owned by user2, then lock down public and grant all rights
 CREATE DATABASE db2 OWNER user2;
